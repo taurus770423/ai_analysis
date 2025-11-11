e06fe7cd3f46779b4c4e4c9a83bdd410ce6172ad
taurus770423 <taurus770423@gmail.com>
2025-11-11 09:07:27 +0800

    feat

--- winmon_group_buy_frontend/vite.config.ts
+++ winmon_group_buy_frontend/vite.config.ts
@@ -7,8 +7,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    port: 5173,
     allowedHosts: [
       'winmon.co'
-    ]
+    ],
+    hmr: {
+      port: 443,         // Nginx 正在監聽的 SSL port
+      protocol: 'wss'    // 使用 Secure WebSocket
+    }
   }
 })
