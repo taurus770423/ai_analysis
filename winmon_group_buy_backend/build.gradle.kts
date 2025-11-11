3f429f2fa9b4db17b9d1f8fdff3a6a435c471259
taurus770423 <taurus770423@gmail.com>
2025-11-11 10:00:59 +0800

    feat

--- winmon_group_buy_backend/build.gradle.kts
+++ winmon_group_buy_backend/build.gradle.kts
@@ -30,11 +30,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
 
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 	runtimeOnly("org.postgresql:postgresql")
+
+	implementation("org.bouncycastle:bcprov-jdk18on:1.82")
 
 	compileOnly("org.projectlombok:lombok")
 
